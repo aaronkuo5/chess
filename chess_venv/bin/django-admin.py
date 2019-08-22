a61a71055c5f73d1672490e5c1d88bffb261e450
Aaron Kuo <aaronkuo@Aaron-Kuos-MacBook.local>
2019-08-22 21:25:51 +0800

    removing excess files

--- chess_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/aaronkuo/Desktop/2019Summer/PersonalStuff/chess/chess_venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
